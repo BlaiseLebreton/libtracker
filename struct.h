3c1e2fbdad274e9cb7dc72b6b110f23297e2b0ed
BlaiseLebreton <blaise.lebreton@sfr.fr>
2021-07-17 22:37:16 +0200

    Update comments

--- struct.h
+++ struct.h
@@ -11,22 +11,23 @@ using namespace std;
   struct tracker_params {
     float distth  = 20;   // Distance threshold for new track
     int   klost   = 20;   // Number of frame after a track is deleted if lost
-    float c_cr    = 1.0;
-    float c_tl    = 0.0;
-    float c_br    = 0.0;
+
+    // Coefficient for distance to :
+    float c_cr    = 1.0;  // Center
+    float c_tl    = 0.0;  // Top-left     corner
+    float c_br    = 0.0;  // Bottom-right corner
   };
 
   // Structure of tracks
   struct track {
-    vector<Point2f> p; // Position
-    Rect rect;         // Bounding box
-    Point2f v;         // Speed
-    Point2f a;         // Acceleration
-    int kfound=0;      // Number of frame track has been updated
-    int klost=0;       // Number of frame track has been lost
-    bool found=true;   // Found corresponding object at frame
-    // Scalar color = Scalar(rng.uniform(0,255), rng.uniform(0, 255), rng.uniform(0, 255)); // Color
-    Scalar color = Scalar(rand() % 255, rand() % 255, rand() % 255); // Color
+    vector<Point2f> p;                                                         // Position
+    Rect            rect;                                                      // Bounding box
+    Point2f         v;                                                         // Speed
+    Point2f         a;                                                         // Acceleration
+    int             kfound = 0;                                                // Number of frame track has been updated
+    int             klost  = 0;                                                // Number of frame track has been lost
+    bool            found  = true;                                             // Found corresponding object at frame
+    Scalar          color  = Scalar(rand() % 255, rand() % 255, rand() % 255); // Color
   };
 
   // Structure of objects
